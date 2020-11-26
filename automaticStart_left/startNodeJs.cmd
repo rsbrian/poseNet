33ec67f5cb313124e37627ee3b4c5976ff58f121
jacky1107 <jacky110711071107@gmail.com>
2020-11-26 15:27:45 +0800

    update auto start_left

---
+++ automaticStart_left/startNodeJs.cmd
@@ -0,0 +1,4 @@
+rem @echo off
+cd C:\Users\User\Documents\openpose\build\examples\poseNet\MOVE
+set NODE_ENV=production
+node bin/www
---
+++ automaticStart_left/startPoseNet.cmd
@@ -0,0 +1,5 @@
+call C:\Users\User\anaconda3\Scripts\activate.bat C:\Users\User\anaconda3
+call conda activate opnet
+cd C:\Users\User\Documents\openpose\build\examples\poseNet
+python opnet.py
+
